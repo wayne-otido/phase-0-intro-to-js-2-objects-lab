894c9193abd3cbedcce2e3f8adbbed79f2384f6b
Wayne Otido <otidowayne@gmail.com>
2023-06-18 11:11:09 +0300

    started assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+let employee = {
+  name: "John",
+  streetAddress : "Kilimani"
+}
+console.log(employee)
+function updateEmployeeWithKeyAndValue() {
+  employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+  }
+  console.log(employee)
+  return employee
+}
+updateEmployeeWithKeyAndValue()
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+  
+}
